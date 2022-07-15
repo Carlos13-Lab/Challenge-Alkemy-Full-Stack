32c8fd9d8c2dfcb8d0c65468e091f077b41149d8
Carlos <cddd3871@gmail.com>
2022-07-15 08:51:46 -0400

    [ADD] Binder Client, [UPDATE] Name binder api

---
+++ Client/src/routes/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import '../styles/index.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>HOlA</h1>
+    </div>
+  )
+}
+
+export default App
